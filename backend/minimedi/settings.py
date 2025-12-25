c47b66346a84735b1a918b0f65c5045abef42e90
Lohit534 <22h41a0534lohit@gmail.com>
2025-12-25 15:19:17 +0530

    feat: configure Django project settings including database, apps, middleware, CORS, and REST framework.

--- backend/minimedi/settings.py
+++ backend/minimedi/settings.py
@@ -23,7 +23,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
-    ".onrender.com",
+    "https://minimedi.onrender.com/",
 ]
 
 # =========================
